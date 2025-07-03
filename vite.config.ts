700d910a0b7193595589e8f5f72549c34a37e074
Alfredo Allan <allan-mecanico@outlook.com>
2025-07-02 22:35:50 -0300

    Apagando dependencias do backend

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./client/src', import.meta.url)),
-      '@shared': fileURLToPath(new URL('./shared', import.meta.url)),
       '@assets': fileURLToPath(new URL('./attached_assets', import.meta.url)),
     },
   },
